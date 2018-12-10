bb2b25e828daaafd4980b67aab44debcfaced998
elkarrde <nfalica@gmail.com>
2018-12-10 16:36:03 +0100

    Click handlers

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/code')).listen(process.env.PORT || 8080, function(){
-    console.log('Server running on %s..', process.env.PORT || 8080);
+    console.log('Server running on %s...', process.env.PORT || 8080);
 });
